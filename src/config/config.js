f72be02f320ff4cd01ae1afa38aa1c3ad39f371b
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-09-04 22:49:22 +0530

    after fixing notes in contact

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-// export const serverURL = "http://localhost:1234"
+export const serverURL = "http://localhost:1234"
 // export const serverURL = "https://crm-backend-liard.vercel.app"
-export const serverURL = "https://crm-backend-k0mi.onrender.com"
+// export const serverURL = "https://crm-backend-k0mi.onrender.com"
