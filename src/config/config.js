f97ad2b04530f88b10e09ac46ef8a6024e8d4b21
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-09-05 18:50:36 +0530

    vercel test 2

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 // export const serverURL = "http://localhost:1234"
 export const serverURL = "https://crm-backend-liard.vercel.app"
+// export const serverURL = "https://crm-backend-46sy1b39t-ashwin-gorles-projects.vercel.app/"
 // export const serverURL = "https://crm-backend-k0mi.onrender.com"
