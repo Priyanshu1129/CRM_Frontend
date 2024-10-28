c3050f9c2c5e23fcccc9bbf62ce8e04731aab2f9
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-10-28 20:17:13 +0530

    After Change server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-export const serverURL = "http://localhost:4321"
+// export const serverURL = "http://localhost:4321"
+export const serverURL = "https://crm-dashboard-backend-zrjm.vercel.app" // server 2 url
 // export const serverURL = "https://crm-backend-liard.vercel.app"
 // export const serverURL = "https://crm-backend-46sy1b39t-ashwin-gorles-projects.vercel.app"
 // export const serverURL = "https://crm-backend-k0mi.onrender.com"
