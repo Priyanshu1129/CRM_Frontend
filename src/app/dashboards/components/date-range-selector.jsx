050f8fb7cc8c6af9b58b217a313687085d96efb8
Priyanshu1129 <priyanshujaj11@gmail.com>
2024-11-26 20:24:31 +0530

    range picker icon fixed

--- src/app/dashboards/components/date-range-selector.jsx
+++ src/app/dashboards/components/date-range-selector.jsx
@@ -1,43 +1,26 @@
-// import React from "react";
-// import { DatePicker } from "antd";
-// import dayjs from "dayjs";
-// import { CalendarOutlined } from "@ant-design/icons";
-// import { colorConfig } from "@/config";
-
-// const { RangePicker } = DatePicker;
-
-// export const SelectDateRange = ({ onChange }) => (
-//   <RangePicker
-//     onChange={onChange}
-//     defaultValue={[dayjs("2015/01/01"), dayjs(new Date())]}
-//     format={"DD-MM-YYYY"}
-//     needConfirm
-//     allowClear={false}
-//     suffixIcon={
-//       <CalendarOutlined style={{ color: `${colorConfig.primary}` }} /> // Set the color of the calendar icon
-//     }
-//   />
-// );
-
 import React from "react";
-import { DatePicker , Space} from "antd";
+import { DatePicker, Space } from "antd";
 import dayjs from "dayjs";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
-export const SelectDateRange = ({ setStartDate, setEndDate }) => 
-  {
-   return ( <Space>
-  <DatePicker
-    onChange={(date, dateString)=>setStartDate(dateString)}
-    defaultValue={dayjs("2020-10-10", "YYYY-MM-DD")}
-    format={"YYYY-MM-DD"}
-    needConfirm
-    allowClear={false}
-  />
-    <DatePicker
-    onChange={(date, dateString)=> setEndDate(dateString)}
-    defaultValue={dayjs(new Date())}
-    format={"YYYY-MM-DD"}
-    needConfirm
-    allowClear={false}
-  />
-  </Space>)};
+export const SelectDateRange = ({ setStartDate, setEndDate }) => {
+  return (
+    <Space>
+      <DatePicker
+        onChange={(date, dateString) => setStartDate(dateString)}
+        defaultValue={dayjs("2020-10-10", "YYYY-MM-DD")}
+        format={"YYYY-MM-DD"}
+        needConfirm
+        allowClear={false}
+      />
+      <ArrowRightOutlined  />
+      <DatePicker
+        onChange={(date, dateString) => setEndDate(dateString)}
+        defaultValue={dayjs(new Date())}
+        format={"YYYY-MM-DD"}
+        needConfirm
+        allowClear={false}
+      />
+    </Space>
+  );
+};
