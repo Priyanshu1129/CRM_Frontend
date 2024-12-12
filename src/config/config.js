108580c69c854d2d5fb3cd83c32b3bea734e3f47
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-12-12 17:59:25 +0530

    db url changed

--- src/config/config.js
+++ src/config/config.js
@@ -12,6 +12,6 @@
 
 
 
-export const serverURL = "http://localhost:4321"
+// export const serverURL = "http://localhost:4321"
 
-// export const serverURL = "https://crm-dashboard-backend.vercel.app" // server
+export const serverURL = "https://crm-dashboard-backend.vercel.app" // server
