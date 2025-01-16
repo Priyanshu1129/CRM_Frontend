ab91f6b37f8dd0408e991dfe4ee8ecf1d6e20abe
priyanshu1129 <priyanshujaj11@gmail.com>
2025-01-16 17:51:55 +0530

    forgotPassword file err fixed

--- src/hooks/auth/index.js
+++ src/hooks/auth/index.js
@@ -1,6 +1,6 @@
-export * from "./useForgotPassword.js";
 export * from "./useLogout";
 export * from "./useChangePassword";
 export * from "./useNewPassword";
 export * from "./useVerifyOTP";
 export * from "./useLogin.js";
+export * from "./useForgotPassword";
